c1e8b3ec067c09f5885c9c93d304a15bf5388b4c
小峰 <dwfn3398@corp.netease.com>
2021-01-21 10:56:37 +0800

    Update eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,30 @@ import path from 'path'
 import Koa from 'koa'
 import koaStatic from 'koa-static'
 
+const indexPaths = ['/', '/index.html']
 let indexBuffer: Buffer
 
-export default function(options: {path: string, test?: RegExp}): (ctx: Koa.Context, next: () => Promise<any>) => void {
+export default function (options: {
+  path: string
+  test?: RegExp
+}): (ctx: Koa.Context, next: () => Promise<any>) => void {
   return async (ctx, next) => {
-    if (options.test && !options.test.test(ctx.path)) {
-      await next()
-    } else {
+    if (options.test && !options.test.test(ctx.path)) return next()
+
+    if (!indexPaths.includes(ctx.path)) {
       await koaStatic(path.resolve(options.path), {
         maxage: 31536000000
       })(ctx, next)
-      if (ctx.status !== 200) {
-        if (!indexBuffer) {
-          const indexPath = path.resolve(options.path, 'index.html')
-          indexBuffer = fs.readFileSync(indexPath)
-        }
-        ctx.type = 'html'
-        ctx.body = indexBuffer
-      }
     }
+
+    if (ctx.status === 200) return
+    if (!indexBuffer) {
+      const indexFilePath = path.resolve(options.path, 'index.html')
+      if (fs.existsSync(indexFilePath)) indexBuffer = fs.readFileSync(indexFilePath)
+    }
+
+    if (!indexBuffer) return next()
+    ctx.type = 'html'
+    ctx.body = indexBuffer
   }
 }
